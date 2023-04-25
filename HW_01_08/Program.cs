cb8148cc631df9793683fb0bc4c2ae30d10ca150
Dmitrii_Karpechenkov <dimaniche15@gmail.com>
2023-04-25 18:31:04 +0300

    wo massive

--- HW_01_08/Program.cs
+++ HW_01_08/Program.cs
@@ -3,17 +3,12 @@ using System;
 
 string numbers = Console.ReadLine();
 int num = Convert.ToInt32(numbers);
-int[] nums = new int[num];
 
-for(int i = 0; i < num;i++) // заполнение массива
-{
-    nums[i] = i;
-}
 for(int i = 1; i < num; i++)
 {
     if(i % 2 == 0)
     {
-    Console.WriteLine(nums[i]);
+        Console.WriteLine(i);
     }
 }
 
