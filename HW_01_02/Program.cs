408e8222a9f32c4611032bd6ac642bfd244ea806
Dmitrii_Karpechenkov <dimaniche15@gmail.com>
2023-04-25 18:07:13 +0300

    new HW

---
+++ HW_01_02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+Console.Write("Numbers = ");
+string[] numbers = Console.ReadLine().Split(',');
+int a = Convert.ToInt32(numbers[0]);
+int b = Convert.ToInt32(numbers[1]);
+int max = 0;
+
+if(a>b)
+{
+    max = a;
+}
+else
+{ 
+    max  = b;
+}
+Console.Write("max -> ");
+Console.WriteLine(max);
---
+++ HW_01_06/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string[] number = Console.ReadLine().Split(',');
+int a = Convert.ToInt32(number[0]);
+int d = a % 2;
+bool truly;
+
+if(d == 0)
+{
+    truly = true;
+    Console.Write(a);
+    Console.Write(" -> Да");
+    
+}
+else
+{
+    Console.Write(a);
+    Console.Write(" -> Нет");
+}
