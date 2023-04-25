e3d11ef3eda589f76808d5d421f31b7b84f169c9
Dmitrii_Karpechenkov <dimaniche15@gmail.com>
2023-04-25 18:08:49 +0300

    new HW

---
+++ HW_01_04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+Console.Write("Numbers = ");
+string[] numbers = Console.ReadLine().Split(',');
+int a = Convert.ToInt32(numbers[0]);
+int b = Convert.ToInt32(numbers[1]);
+int c = Convert.ToInt32(numbers[2]);
+int max = 0;
+
+if(a>max)
+{
+    max = a;
+}
+if(b>max)
+{
+    max = b;
+}
+if(c>max)
+{
+    max = c;
+}
+Console.Write("max -> ");
+Console.Write(max);
