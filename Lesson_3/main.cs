7bc72ecf87090ae7dcb04e092c5b1aea77ad4acc
Dmitrii_Karpechenkov <dimaniche15@gmail.com>
2023-05-04 15:06:31 +0300

    edit

--- Lesson_3/main.cs
+++ Lesson_3/main.cs
@@ -57,14 +57,6 @@ Print(number);
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
 
-// Напишите программу, которая принимает на вход координаты 
-// двух точек и находит расстояние между ними в 3D пространстве.
-
-// A (3,6,8); B (2,1,-7), -> 15.84
-
-// A (7,-5, 0); B (1,-1,9) -> 11.53
-
-
 double CalculateDistance(int xA, int yA, int zA, int xB, int yB, int zB)
 {   
     double distanceSquared = Math.Abs(Math.Pow((xB - xA),2) + Math.Pow((yB - yA),2) + Math.Pow((zB - zA),2));
